45bf7e8d90c2ae25c32591ece6a71de6df9e210d
Marius Tacke <marius.tacke@hereon.de>
2024-11-14 15:53:29 +0100

    Computed loss per loading condition

--- cann_llm_comparison/scientific_generative_agent.py
+++ cann_llm_comparison/scientific_generative_agent.py
@@ -48,21 +48,21 @@ class ScientificGenerativeAgent():
                         print("Max attempts reached. Operation failed.")
                         return None
             model, model_code = _recursive_generate_and_evaluate_model()
-            loss              = self._evaluator.evaluate(iteration, self._loader, model)
+            loss, loss_line   = self._evaluator.evaluate(iteration, self._loader, model)
 
-            self._save_model(model, model_code, loss)
+            self._save_model(model, model_code, loss, loss_line)
 
-        model, model_code, _ = self._load_best_model()
+        model, model_code, _, _ = self._load_best_model()
         self._exporter.export(self._loader, model, model_code)
 
 
     def _generate_and_evaluate_model(self, system_prompt, user_prompt, fit_code):
         # Outer-Level Optimization: Ask LLM for forward equation based on previous iterations
-        prev = ""
-        for idx, (_, top_k_model_code, top_k_model_loss) in enumerate(self._top_k_models):
-            prev += f"### Previous iteration #{idx}:\n\n{                   top_k_model_code}\n\n"
-            prev += f"### Feedback on previous iteration #{idx}:\n\nLoss = {top_k_model_loss}\n\n"
-        user_prompt = prev + user_prompt
+        previous = ""
+        for idx, (_, top_k_code, _, top_k_loss_line) in enumerate(self._top_k_models):
+            previous += (f"### Previous iteration #{            idx}:\n\n{top_k_code     }\n\n"
+                         f"### Feedback on previous iteration #{idx}:\n\n{top_k_loss_line}\n\n")
+        user_prompt = previous + user_prompt
         messages = [
             {"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}
         ]
@@ -80,8 +80,8 @@ class ScientificGenerativeAgent():
         return model, model_code
 
 
-    def _save_model(self, model, model_code, loss):
-        self._top_k_models.append([copy.deepcopy(model), model_code, loss])
+    def _save_model(self, model, model_code, loss, loss_line):
+        self._top_k_models.append([copy.deepcopy(model), model_code, loss, loss_line])
         # Sort models by loss
         self._top_k_models.sort(key=lambda x: x[2])
         # Keep only the top k models
